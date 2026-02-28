e9e5791dd96ee8d1511eab281b46f412ea37298d
Thomas Peyró <thomas.voeffray@hotmail.com>
2026-02-28 19:09:34 +0100

    Update user interface

--- app/types/user.ts
+++ app/types/user.ts
@@ -1,7 +1,8 @@
 export interface User {
-  id: string | null;
-  name: string | null;
-  username: string | null;
-  token: string | null;
-  status: string | null;
+  id: number;
+  name: string;
+  username: string;
+  bio: string;
+  token?: string;
+  status: "ONLINE" | "OFFLINE" | string;
 }
