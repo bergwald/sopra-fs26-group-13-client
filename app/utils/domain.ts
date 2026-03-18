8199500f39793c4aac06ac63b7884453716073f4
Thomas Peyró <thomas.voeffray@hotmail.com>
2026-03-18 10:23:43 +0100

    Update prod URL

--- app/utils/domain.ts
+++ app/utils/domain.ts
@@ -7,7 +7,7 @@ import { isProduction } from "@/utils/environment";
  */
 export function getApiDomain(): string {
   const prodUrl = process.env.NEXT_PUBLIC_PROD_API_URL ||
-    "https://gen-lang-client-0107477828.oa.r.appspot.com/"; // TODO: update with your production URL as needed.
+    "https://sopra-fs26-group-13-server.oa.r.appspot.com"; // TODO: update with your production URL as needed.
   const devUrl = "http://localhost:8080";
   return isProduction() ? prodUrl : devUrl;
 }
