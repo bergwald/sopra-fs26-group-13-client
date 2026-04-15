35aff4d7ec72d60d7bd00ea49b559b380fc63bff
Thomas Peyró <thomas.voeffray@hotmail.com>
2026-04-15 16:12:07 +0200

    Add link to temp game demo on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
 "use client";
 
+import Link from "next/link";
 import React from "react";
 
+const homepageLinksStyle: React.CSSProperties = {
+  display: "flex",
+  flexDirection: "column",
+  gap: "8px",
+};
+
 const HomePage: React.FC = () => {
-  return <div className="login-container">Homepage</div>;
+  return (
+    <div className="login-container">
+      <div style={homepageLinksStyle}>
+        <div>Homepage</div>
+        <Link href="/game/demo">Game demo</Link>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
