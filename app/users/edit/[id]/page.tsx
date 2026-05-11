9816e148f64b06f6578367425ae28f9eafbbcbbb
Thomas Peyró <thomas.voeffray@hotmail.com>
2026-05-11 19:13:01 +0200

    Replace "G" logo with a mountain emoji

--- app/users/edit/[id]/page.tsx
+++ app/users/edit/[id]/page.tsx
@@ -245,7 +245,7 @@ const UserSettingsPage: React.FC = () => {
         <div className="login-page-nav-left">
           <Link href="/" className="login-page-brand">
             <div className="login-page-brand-icon" aria-hidden="true">
-              G
+              ⛰️
             </div>
             <span className="login-page-brand-text">MountainGuessr</span>
           </Link>
